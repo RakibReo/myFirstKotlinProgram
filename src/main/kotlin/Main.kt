953865316387ee4aeab2bd1fb8742dc370266c8a
SuRakib <rakibul15-10976@diu.edu.bd>
2023-03-14 09:13:17 +0600

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    val x=7
+
+    if(x in 1..5){
+        print("x is range from 1 to 5")
+
+    }
+    else if(x in 6..10){
+
+        print("x is range from 6 to 10")
+
+    } else{
+
+        print("Answer Not Found")
+
+    }
+
+
+
+
+}
